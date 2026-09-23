1a924ad7dbdba548f28ba006fd11b2d9bbf265af
Chilevec <chilevecnull@gmail.com>
2026-09-23 14:56:22 -0700

    Added event cog for message listening, including a "swear filter" ;) Fixed my .gitignore to properly disclude pycache. You'll notice that to add this cog which adds a lot of features, nothing needed changed in main.py. Among many things, behold the modularity!

---
+++ my-discord-bot/cogs/event_cog.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+
+class EventCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        if message.author.bot:
+            return
+
+        # Catches prefix-initiated commands. I think.
+        await self.bot.process_commands(message)
+
+        if "swear" in message.content.lower():
+            await message.channel.send("Hey, you can't say the swear word.")
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(EventCog(bot))
