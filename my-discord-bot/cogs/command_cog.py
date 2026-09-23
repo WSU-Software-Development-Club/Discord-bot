8f237d09b1834daf31b8412146829a38eef1cea3
Chilevec <chilevecnull@gmail.com>
2026-09-23 14:39:13 -0700

    Used the docs to create much more heavy duty architecture. Added some example commands.

---
+++ my-discord-bot/cogs/command_cog.py
@@ -0,0 +1,36 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+class CommandCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command(name="supersecret")
+    async def supersecret(self, ctx: commands.Context) -> None:
+        await ctx.message.delete()
+        if hasattr(self, "spoilsport") and self.spoilsport is ctx.author.id:
+            await ctx.send("Now you're just doing it on purpose...")
+        else:
+            await ctx.send("Shhhh!!! You're going to spoil the secret!")
+            self.spoilsport = ctx.author.id
+
+    @app_commands.command(name="hello", description="Say hello to our new friend! If you value your soul.")
+    async def hello(self, integration: discord.Integration) -> None:
+        await integration.response.send_message("Hello!")
+
+    @commands.hybrid_command(name="sync", description="Sync commands to your guild.")
+    async def sync(self, ctx: commands.Context) -> None:
+        if ctx.guild is None:
+            ctx.send("This command is only for server slash-command syncing.")
+            return
+
+        await ctx.defer(ephemeral=True)
+
+        self.bot.tree.copy_global_to(guild=ctx.guild)
+        synced = await self.bot.tree.sync(guild=ctx.guild)
+
+        await ctx.send(f"Synced {len(synced)} command(s).", ephemeral=True)
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(CommandCog(bot))
--- my-discord-bot/main.py
+++ my-discord-bot/main.py
@@ -1,27 +1,73 @@
-import discord, os
+import datetime, logging, os, traceback, typing
+
+import aiohttp, discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
-load_dotenv()
+class WSUBot(commands.Bot):
+    client: aiohttp.ClientSession
+    _uptime: datetime.datetime = datetime.datetime.now(datetime.UTC)
 
-intents = discord.Intents.default()
-intents.message_content = True
+    def __init__(self, prefix: str, ext_dir: str, *args: typing.Any, **kwargs: typing.Any) -> None:
+        intents = discord.Intents.default()
+        intents.members = True
+        intents.message_content = True
+        super().__init__(*args, **kwargs, command_prefix=commands.when_mentioned_or(prefix), intents=intents)
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.ext_dir = ext_dir
+        self.synced = False
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+    async def _load_extensions(self) -> None:
+        if not os.path.isdir(self.ext_dir):
+            self.logger.error(f"Extension directory '{self.ext_dir}' does not exist.")
+            return
+        for filename in os.listdir(self.ext_dir):
+            if filename.endswith(".py") and not filename.startswith("_"):
+                try:
+                    await self.load_extension(f"cogs.{filename[:-3]}")
+                    self.logger.info(f"Loaded extension {filename[:-3]}")
+                except commands.ExtensionError:
+                    self.logger.error(f"Failed to load extension {filename[:-3]}\n{traceback.format_exc()}")
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('$'), intents=intents)
+    async def on_error(self, event_method, *args, **kwargs) -> None:
+        self.logger.exception(f"An error occurred in {event_method}.\n{traceback.format_exc()}")
 
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    async def on_ready(self) -> None:
+        self.logger.info(f"Logged in as {self.user} (ID: {self.user.id})")
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+    async def setup_hook(self) -> None:
+        self.client = aiohttp.ClientSession()
+        await self._load_extensions()
+        # if not self.synced:
+        #     await self.tree.sync()
+        #     self.synced = not self.synced
+        #     self.logger.info("Synced command tree.")
 
-@bot.tree.command()
-async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(f'Hello {interaction.user.name}!')
+    async def close(self) -> None:
+        await super().close()
+        await self.client.close()
+    
+    def run (self, *args: typing.Any, **kwargs: typing.Any) -> None:
+        load_dotenv()
+        try:
+            super().run(str(os.getenv("BOT_TOKEN")), *args, **kwargs)
+        except (discord.LoginFailure, KeyboardInterrupt):
+            self.logger.info("Exiting...")
+            exit()
+    
+    @property
+    def user(self) -> discord.ClientUser:
+        assert super().user, "Bot is not ready yet."
+        return typing.cast(discord.ClientUser, super().user)
 
-bot.run(BOT_TOKEN)
+    @property
+    def uptime(self) -> datetime.timedelta:
+        return datetime.datetime.now(datetime.UTC) - self._uptime
+
+def main() -> None:
+    # logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+    bot = WSUBot(prefix="$", ext_dir="my-discord-bot/cogs")
+    bot.run()
+
+if __name__ == "__main__":
+    main()
